c29924cac81df21f4f996d8ab35d8cc14e04e9e4
lightyears <lightyears1998@hotmail.com>
2018-10-14 23:11:12 +0800

    ç¦å°æ©æ¯ççº¦ä¼

--- Nowcoder/PAT乙级(Basic Level)真题/1004.cpp
+++ Nowcoder/PAT乙级(Basic Level)真题/1004.cpp
@@ -1,59 +1,52 @@
-// WA
+// 福尔摩斯的约会
+// Warning.
 
 #include <bits/stdc++.h>
 using namespace std;
 
-string a, b, c, d;
-int cnt; string ans;
+string a, b, c, d, ans;
 
 const string days[] = {
   "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"
 };
 
-void comp(const string& a, const string &b)
-{
-  for (unsigned i = 0; i < min(a.length(), b.length()); ++i)
-  {
-    if (a[i] == b[i]) {
-      if (!isalpha(a[i])) continue;
-      switch(cnt) {
-        case 0: {
-          ans += days[tolower(a[i]) - 'a'];
-          ans += ' ';
-          break;
-        }
-        case 1: {
-          int num = i;
-          if (num >= 10) {
-            ans += '1';
-            num -= 10;
-          }
-          else ans += '0';
-          ans += char('0' + num);
-          ans += ':';
-          break;
-        }
-        case 2: {
-          int num = i;
-          if (num >= 10) {
-            ans += '1';
-            num -= 10;
-          }
-          else ans += '0';
-          ans += char('0' + num);
-          break;
-        }
-      }
-      ++cnt;
-    }
-  }
-  cout << endl;
-}
-
 signed main()
 {
   cin >> a >> b >> c >> d;
-  comp(a, b);
-  comp(c, d);
-  cout << ans;
+
+  for (unsigned i = 0, cnt = 0; i < min(a.length(), b.length()); ++i)
+  {
+    if (cnt == 0 && isupper(a[i]) && a[i] == b[i])
+    {
+      ans += days[a[i] - 'A'] + " "; ++cnt;
+      continue;
+    }
+    if (cnt == 1 && a[i] == b[i])
+    {
+      if (isdigit(a[i])) {
+        ans += string("0") + a[i];
+        // 这里不能写成 ans += "0" + a[i]，赋值号右边不能确认类型，string::append报错
+      }
+      else {
+        char ch = toupper(a[i]);
+        int clock = ch - 'A' + 10;
+        ans += to_string(clock);
+      }
+      break;
+    }
+  }
+  
+  ans += ":";
+
+  for (unsigned i = 0; i < min(c.length(), d.length()); ++i)
+  {
+    if (isalpha(c[i]) && c[i] == d[i]) {
+      if (i < 10)
+        ans += "0";
+      ans += to_string(i);
+      break;
+    }
+  }
+
+  cout << ans << endl;
 }
